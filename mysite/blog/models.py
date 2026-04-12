af6044337fdcd83e0402423dd4e8ae6f446c177b
samiZZZ67 <abiyafikre@gmail.com>
2026-04-12 10:47:56 +0300

    Add blog post functionality including models updates, list and detail views, URL routing, and templates

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,6 +4,11 @@ from django.utils import timezone
 
 # Create your models here.
 from django.db import models
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -26,6 +31,8 @@ class Post(models.Model):
     choices=Status,
     default=Status.DRAFT
     )
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager
        
     class Meta:
         ordering=['-publish']
