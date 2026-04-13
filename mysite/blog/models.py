c767a6f63ea4812fefd3fe82d98e42ed56e11466
samiZZZ67 <abiyafikre@gmail.com>
2026-04-13 15:53:10 +0300

    Implement canonical URL support; refactor list template, models, URLs, and views

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
 from django.db import models
@@ -15,7 +16,9 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
         
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish')
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -38,3 +41,14 @@ class Post(models.Model):
         ordering=['-publish']
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse(
+        'blog:post_detail',
+        args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+)
