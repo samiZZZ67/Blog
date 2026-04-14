1003bea74442e6049a5fb5aa36ade25b81000dbf
samiZZZ67 <abiyafikre@gmail.com>
2026-04-14 16:10:25 +0300

    clean views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -12,26 +12,7 @@ class Postlistview(ListView):
     context_object_name='posts'
     paginate_by=3
     template_name='blog/post/list.html'
-# Create your views here.
-# def post_list(request):
-#     post_list = Post.published.all()
 
-#     paginator=Paginator(post_list,3)
-#     page_number=request.GET.get('page',1)
-#     try:
-#         posts=paginator.page(page_number)
-#     except EmptyPage:
-#         posts=paginator.page(paginator.num_pages)
-#     except PageNotAnInteger:
-#         posts=paginator.page(1)
-#         raise PageNotAnInteger('only number')
-        
-
-#     return render(
-#         request,
-#         'blog/post/list.html',
-#         {'posts': posts}
-#     )
 
 def post_detail(request, year, month, day, publish):
  
