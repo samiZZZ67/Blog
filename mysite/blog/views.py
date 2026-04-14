edbbfb171ed9699343a3503a17afc8120db0b561
samiZZZ67 <abiyafikre@gmail.com>
2026-04-14 18:23:35 +0300

    add forms for email

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,7 @@ from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 from django.views.generic import ListView
 from django.shortcuts import render,get_object_or_404
 from django.http import Http404
-
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -27,6 +27,29 @@ def post_detail(request, year, month, day, publish):
         request,
         'blog/post/detail.html',
         {'post': post}
+    )
+
+def post_share(request,post_id):
+    post=get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+
+    if request.method=='POST':
+        form=EmailPostForm(request.POST)
+
+        if form.is_valid:
+            cd=form.cleaned_data
+    else:
+        form=EmailPostForm()
+
+    return render (
+        request,
+        'blog/post/share.html',
+        {'forms':form},
+        {'post':post}
+
     )
 
 
