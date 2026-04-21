f22ce0feab6e32c7678405c42241d8f875f406b2
samiZZZ67 <abiyafikre@gmail.com>
2026-04-21 19:58:12 +0300

    Refactor views and URLs: add user-friendly pagination, improve error handling, and convert class-based views to function-based views

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,8 +5,10 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     # post views
-    path('', views.PostListView.as_view(), name='post_list'),
-    path('tag/<slug:tag_slug>/',views.PostListView.as_view(), name='post_list_by_tag'),
+    path('', views.post_list, name='post_list'),
+    path('tag/<slug:tag_slug>/',views.post_list, name='post_list_by_tag'),
+    # path('', views.PostListView.as_view(), name='post_list'),
+    # path('tag/<slug:tag_slug>/',views.PostListView.as_view(), name='post_list_by_tag'),
     path(
     '<int:year>/<int:month>/<int:day>/<slug:publish>/',
     views.post_detail,
