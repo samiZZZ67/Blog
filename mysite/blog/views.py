b12602b4ef8f7e9027a2484113c43a5bfa4878ab
samiZZZ67 <abiyafikre@gmail.com>
2026-04-24 19:39:49 +0300

    add simple tag for how many post available and inclusion tag for latest posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,6 +7,7 @@ from .forms import EmailPostForm,CommentForm
 from .models import Post,Comment
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -46,12 +47,21 @@ def post_detail(request, year, month, day, publish):
         publish__day=day)
     comments=post.comments.filter(active=True)
     form=CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
+
     return render(
         request,
         'blog/post/detail.html',
         {'post': post,
          'form':form,
-         'comments':comments}
+         'comments':comments,
+         'similar_posts':similar_posts,}
     )
 @require_POST
 def post_comment(request,post_id):
